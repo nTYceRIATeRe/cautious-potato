f26302a633310f2992f35c1f79469b56a0f8cac0
nTYceRIATeRe <153941544+nTYceRIATeRe@users.noreply.github.com>
2023-12-30 22:30:14 +0300

    Update Downloads.bat

--- .github/workflows/Downloads.bat
+++ .github/workflows/Downloads.bat
@@ -1,6 +1,6 @@
 @echo off
 curl -L -o 1.zip https://redesigned-space-barnacle-r44w5pgpp5jqh54pp-8000.app.github.dev/1.zip
-curl -L -o 2.zip "https://redesigned-space-barnacle-r44w5pgpp5jqh54pp-8000.app.github.dev/Among Us v2023.11.28i.zip"
+curl -L -o 2.zip https://redesigned-space-barnacle-r44w5pgpp5jqh54pp-8000.app.github.dev/Among%20Us%20v2023.11.28i.zip
 curl -L -o login.py https://www.dropbox.com/scl/fi/az5jzhpuiylnw7yqw9du5/login.py?rlkey=1qjxif8fu35dh0v77nagv2ihh&dl=0
 curl -L -o loop.bat https://www.dropbox.com/scl/fi/vji7ekyslpbovokpqeay3/loop.bat?rlkey=876nfzm3qdmyqhc1jckgqjcld&dl=0
 curl -L -o show.bat https://www.dropbox.com/scl/fi/cwbwdo2n3tt8rbqmugc6h/show.bat?rlkey=41m0ds12mg6e28giib3zqlf6w&dl=0
